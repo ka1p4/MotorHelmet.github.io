b8e6bac33b74b68c1a1c8cf010678bd78eb34952
Yuki <yukiharuka@outlook.com>
2021-11-26 20:54:32 +0900

    Accelmotor and RelayServer.js

---
+++ accel.js
@@ -0,0 +1,31 @@
+import {requestI2CAccess} from "./node_modules/node-web-i2c/index.js";
+import GROVEACCELEROMETER from "@chirimen/grove-accelerometer";
+import nodeWebSocketLib from "websocket";
+import {RelayServer} from "./RelayServer.js";
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+
+
+const AccelMotorSend = "AccelMotorSend";
+
+main();
+
+async function main() {
+    const i2cAccess = await requestI2CAccess();
+    const port = i2cAccess.ports.get(1);
+    const groveaccelerometer = new GROVEACCELEROMETER(port, 0x53);
+    await groveaccelerometer.init();
+
+	let relay = RelayServer("achex", "chirimenSocket" , nodeWebSocketLib, "https://chirimen.org");
+    let channel = await relay.subscribe(AccelMotorSend);
+
+    for (;;) {
+        try {
+        const values = await groveaccelerometer.read();
+        console.log(`ax: ${values.x}, ax: ${values.y}, ax: ${values.z}\nAll: ${Math.abs(values.x) + Math.abs(values.y)}`);
+        channel.send({ax: values.x, ay: values.y, az: values.z});
+        } catch (err) {
+            console.error("READ ERROR:" + err);
+        }
+        await sleep(1000);
+    }
+}
